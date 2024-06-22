0de79db2dabc169719a9708f835dff21a9819c54
JoshLovesCodes <67225910+JoshLovesCodes@users.noreply.github.com>
2024-06-22 21:07:15 +0800

    finished

--- VERSION 2 (MVC)/Reservation.java
+++ VERSION 2 (MVC)/Reservation.java
@@ -2,15 +2,26 @@ public class Reservation {
     private Guest guest;
     private Date checkInDate, checkOutDate;
     private Room room;
+    private Fees fees;
+    private int totalDays;
 
-    public Reservation(Guest guest, Date checkInDate, Date checkOutDate, Room room) {
+    public Reservation(Guest guest, Date checkInDate, Date checkOutDate, Room room, Fees fees) {
         this.guest = guest;
         this.checkInDate = checkInDate;
         this.checkOutDate = checkOutDate;
         this.room = room;
+        this.fees = fees;
+        this.totalDays = this.checkOutDate.getDay - this.checkInDate.getDay;
     }
 
     public double getTotalPrice() {
-        
+        if(this.totalDays == 0)
+            return this.room.getPrice + this.fees.getTotalFees;
+        else 
+            return this.room.getPrice * this.totalDays + this.fees.getTotalFees;
+    }
+
+    public double getCostPerNight() {
+        return this.getTotalPrice / this.totalDays;
     }
 }
