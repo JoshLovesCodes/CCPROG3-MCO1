347a59f41fb32f474377561e0413c0777712d147
JoshLovesCodes <67225910+JoshLovesCodes@users.noreply.github.com>
2024-06-22 21:06:57 +0800

    removed parking fee (for MCO2 nalang)

--- VERSION 2 (MVC)/Fees.java
+++ VERSION 2 (MVC)/Fees.java
@@ -1,31 +1,24 @@
 public class Fees {
     private double bookingFee, baseParkingFee, amenitiesFee;
 
-    public Fees(double bookingFee, double baseParkingFee, double amenitiesFee) {
+    public Fees(double bookingFee, double amenitiesFee) {
         this.bookingFee = bookingFee;
-        this.baseParkingFee = baseParkingFee;
         this.amenitiesFee = amenitiesFee;
     }
 
     public Fees(){
-        this(50.00, 30.00, 150.00);
+        this(50.00, 150.00);
     }
 
     public double getBookingFee() {
         return this.bookingFee;
     }
 
-    public double getBaseParkingFee() {
-        return this.baseParkingFeeFee;
+    public double getAmenitiesFee() {
+        return this.amenitiesFeeFee;
     }
 
-    public double getTotalParkingFee() {
-        return this.bookingFee;
+    public double getTotalFees() {
+        return this.bookingFee + this.amenitiesFee;
     }
-
-    public double getBookingFee() {
-        return this.bookingFee;
-    }
-
-
 }
