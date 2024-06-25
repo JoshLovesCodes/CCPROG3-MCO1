9945b2657cb6f73392b726e548c6355ed2e42e39
JoshLovesCodes <67225910+JoshLovesCodes@users.noreply.github.com>
2024-06-25 21:32:53 +0800

    finished

--- VERSION 2 (MVC)/HotelManager.java
+++ VERSION 2 (MVC)/HotelManager.java
@@ -8,35 +8,201 @@ public class HotelManager {
     }
 
     public void displayInterface() {
-        int input = 0;
-        String name = "";    // hotel name
-        Hotel hotel = this.model.selectHotel(name);  // SELECT HOTEL NAME
-        int size = 0;
-        String firstName = ""; 
-        String lastName = "";
+        int input;
+        Hotel hotel = this.model.selectHotel(this.view.promptHotelName());
 
         if(hotel != null) {
+
             switch(input) {
-                case HotelManagerView.CHANGE_NAME:
-                    this.model.changeHotelName(hotel, name);
-                    break;
-                case HotelManagerView.ADD_ROOM:
-                    hotel.addRoom();
-                    hotel.addRoom(size);
-                    break;
-                case HotelManagerView.REMOVE_ROOM:
-                    hotel.removeRoom(); 
-                    hotel.removeRoom(size); 
-                    break;
-                case HotelManagerView.UPDATE_PRICE:
-                    this.model.updateRoomPrice(hotel, size);
-                    break;
-                case HotelManagerView.REMOVE_RESERVATION:
-                    hotel.removeReservation(firstName, lastName);
-                    break;
-                case HotelManagerView.REMOVE_HOTEL:
-                    this.model.removeHotel(hotel);
-                    break;
+                do {
+                    input = this.view.promptInfoMenuResponse();
+
+                    switch(input){
+                        case HotelManagerView.CHANGE_NAME:
+                            HotelViewerView.displayLongDivider();
+                            String newName = this.view.promptHotelNewName();
+                            this.view.displayChangeNameInfo();
+                            HotelViewerView.displayShortDivider();
+                            char confirmation = this.view.promptConfirmation();
+                            if(confirmation == 'Y' || confirmation == 'y') {
+                                boolean status = this.model.changeHotelName(hotel, name);
+                                if(status) {
+                                    this.view.displayChangeNameSuccess();
+                                    HotelViewerView.displayLongDivider();
+                                    break;
+                                } else {
+                                    this.view.displayChangeNameFailed();
+                                    HotelViewerView.displayLongDivider();
+                                    break;
+                                }
+                            } else if(confirmation == 'N' || confirmation == 'n') {
+                                HotelViewerView.displayLongDivider();
+                                break;
+                            }
+                        case HotelManagerView.ADD_ROOM:
+                            HotelViewerView.displayLongDivider();
+                            char response = this.view.promptAddingRoom();
+                            char confirmation;
+                            if(response == 'S' || response == 's') {
+                                this.view.displayAddSingleRoom();
+                                HotelViewerView.displayShortDivider();
+                                confirmation = this.view.promptConfirmation();
+                                if(confirmation == 'Y' || confirmation == 'y') {
+                                    boolean status = hotel.addRoom();
+                                    if(status) {
+                                        this.view.displayAddRoomSuccess();
+                                        HotelViewerView.displayLongDivider();
+                                        break;
+                                    } else {
+                                        this.view.displayAddRoomFailed();
+                                        HotelViewerView.displayLongDivider();
+                                        break;
+                                    }
+                                } else if(confirmation == 'N' || confirmation == 'n'){
+                                    HotelViewerView.displayLongDivider();
+                                    break;
+                                }
+                            } else if(response == 'M' || response == 'm') {
+                                int size = promptNumberOfRooms();
+                                this.view.displayAddMultipleRoom(size);
+                                HotelViewerView.displayShortDivider();
+                                confirmation = this.view.promptConfirmation();
+                                if(confirmation == 'Y' || confirmation == 'y') {
+                                    boolean status = hotel.addRoom(size);
+                                    if(status) {
+                                        this.view.displayAddMultipleRoomSuccess(size);
+                                        HotelViewerView.displayLongDivider();
+                                        break;
+                                    } else {
+                                        this.view.displayAddMultipleRoomFailed();
+                                        HotelViewerView.displayLongDivider();
+                                        break;
+                                    }
+                                } else if(confirmation == 'N' || confirmation == 'n'){
+                                    HotelViewerView.displayLongDivider();
+                                    break;
+                                }
+                            }
+                        case HotelManagerView.REMOVE_ROOM:
+                            HotelViewerView.displayLongDivider();
+                            char response = this.view.promptRemovingRoom();
+                            char confirmation;
+                            if(response == 'S' || response == 's') {
+                                this.view.displayRemoveSingleRoom();
+                                HotelViewerView.displayShortDivider();
+                                confirmation = this.view.promptConfirmation();
+                                if(confirmation == 'Y' || confirmation == 'y') {
+                                    boolean status = hotel.removeRoom();
+                                    if(status) {
+                                        this.view.displayRemoveRoomSuccess();
+                                        HotelViewerView.displayLongDivider();
+                                        break;
+                                    } else {
+                                        this.view.displayRemoveRoomFailed();
+                                        HotelViewerView.displayLongDivider();
+                                        break;
+                                    }
+                                } else if(confirmation == 'N' || confirmation == 'n'){
+                                    HotelViewerView.displayLongDivider();
+                                    break;
+                                }
+                            } else if(response == 'M' || response == 'm') {
+                                int size = promptRemoveNumberOfRooms();
+                                this.view.displayRemoveMultipleRoom(size);
+                                HotelViewerView.displayShortDivider();
+                                confirmation = this.view.promptConfirmation();
+                                if(confirmation == 'Y' || confirmation == 'y') {
+                                    boolean status = hotel.removeRoom(size);
+                                    if(status) {
+                                        this.view.displayRemoveMultipleRoomSuccess(size);
+                                        HotelViewerView.displayLongDivider();
+                                        break;
+                                    } else {
+                                        this.view.displayRemoveMultipleRoomFailed();
+                                        HotelViewerView.displayLongDivider();
+                                        break;
+                                    }
+                                } else if(confirmation == 'N' || confirmation == 'n'){
+                                    HotelViewerView.displayLongDivider();
+                                    break;
+                                }
+                            }
+                        case HotelManagerView.UPDATE_PRICE:
+                            HotelViewerView.displayLongDivider();
+                            int newPrice = this.view.promptNewPrice();
+                            char confirmation;
+                            if(newPrice < 100.0){
+                                this.view.displayPromptPriceInvalid();
+                                break;
+                            } else {
+                                this.view.displayUpdatePriceInfo();
+                                HotelViewerView.displayShortDivider();
+                                confirmation = this.view.promptConfirmation();
+                                if(confirmation == 'Y' || confirmation == 'y') {
+                                    boolean status = this.model.updateRoomPrice(hotel, newPrice);
+                                    if(status) {
+                                        this.view.displayUpdatePriceSuccess();
+                                        HotelViewerView.displayLongDivider();
+                                        break;
+                                    } else {
+                                        this.view.displayUpdatePriceFailed();
+                                        HotelViewerView.displayLongDivider();
+                                        break;
+                                    }
+                                } else if(confirmation == 'N' || confirmation == 'n'){
+                                    HotelViewerView.displayLongDivider();
+                                    break;
+                                }
+                            }
+                        case HotelManagerView.REMOVE_RESERVATION:
+                            String firstName = this.view.promptFirstName();
+                            String lastName = this.view.promptLastName();
+                            char confirmation;
+
+                            this.view.displayRemoveReservationInfo();
+                            HotelViewerView.displayShortDivider();
+                            confirmation = this.view.promptConfirmation();
+                            if(confirmation == 'Y' || confirmation == 'y') {
+                                boolean status = hotel.removeReservation(firstName, lastName);
+                                    if(status) {
+                                        this.view.displayRemoveReservationSuccess();
+                                        HotelViewerView.displayLongDivider();
+                                        break;
+                                    } else {
+                                        this.view.displayRemoveReservationFailed();
+                                        HotelViewerView.displayLongDivider();
+                                        break;
+                                    }
+                            } else if(confirmation == 'N' || confirmation == 'n'){
+                                HotelViewerView.displayLongDivider();
+                                break;
+                            }
+                        case HotelManagerView.REMOVE_HOTEL:
+                            char confirmation;
+                            this.view.displayRemoveHotelInfo();
+                            HotelViewerView.displayShortDivider();
+                            confirmation = this.view.promptConfirmation();
+                            if(confirmation == 'Y' || confirmation == 'y') {
+                                boolean status = this.model.removeHotel(hotel);
+                                    if(status) {
+                                        this.view.displayRemoveHotelSuccess();
+                                        HotelViewerView.displayLongDivider();
+                                        break;
+                                    } else {
+                                        this.view.displayRemoveHotelFailed();
+                                        HotelViewerView.displayLongDivider();
+                                        break;
+                                    }
+                            } else if(confirmation == 'N' || confirmation == 'n'){
+                                HotelViewerView.displayLongDivider();
+                                break;
+                            }
+                        case HotelManagerView.MANAGER_EXIT:
+                            input = 7;
+                        default:
+                            System.out.print("INVALID INPUT PLEASE TRY AGAIN");
+                    }
+                } while(input != 7);
             }
         }
     }
--- VERSION 2 (MVC)/HotelManagerView.java
+++ VERSION 2 (MVC)/HotelManagerView.java
@@ -2,9 +2,198 @@ import java.util.Scanner;
 
 public class HotelManagerView {
     private Scanner scanner;
-    public static final int CHANGE_NAME = 1, ADD_ROOM = 2, REMOVE_ROOM = 3, UPDATE_PRICE = 4, REMOVE_RESERVATION = 5, REMOVE_HOTEL = 6;
+    public static final int CHANGE_NAME = 1, ADD_ROOM = 2, REMOVE_ROOM = 3, UPDATE_PRICE = 4, REMOVE_RESERVATION = 5, REMOVE_HOTEL = 6, MANAGER_EXIT = 7;
 
     public HotelManagerView() {
-        
+        this.scanner = new Scanner(System.in);
     }
+
+    public int promptInfoMenuResponse() {
+        System.out.println("What do you want to do?");
+        System.out.println("1) Change Hotel Name");
+        System.out.println("2) Add Room(s)");
+        System.out.println("3) Remove Room(s)");
+        System.out.println("4) Update Room Base Price");
+        System.out.println("5) Remove Reservation");
+        System.out.println("6) Remove Hotel");
+        System.out.println("7) Go Back");
+        System.out.print("Enter your choice here: ");
+
+        return this.scanner.nextInt();
+    }
+    public String promptHotelName() {
+        System.out.println("Enter name of the hotel: ");
+
+        return this.scanner.nextLine();
+    }
+
+    public String promptNewHotelName() {
+        System.out.println("Enter NEW name of the hotel: ");
+
+        return this.scanner.nextLine();
+    }
+
+    public char promptConfirmation() {
+        System.out.println("Do you want to continue? (Y-yes / N-no)");
+
+        return this.scanner.nextChar();
+    }
+
+    public char promptAddingRoom() {
+        System.out.println("Do you want to add single(S) or multiple(M) rooms? ");
+
+        return this.scanner.nextChar();
+    }
+
+    public int promptNumberOfRooms() {
+        System.out.println("Enter number of rooms to be added: ");
+
+        return this.scanner.nextInt();
+    }
+
+    public char promptRemovingRoom() {
+        System.out.println("Do you want to remove single(S) or multiple(M) rooms? ");
+
+        return this.scanner.nextChar();
+    }
+
+    public int promptRemoveNumberOfRooms() {
+        System.out.println("Enter number of rooms to be removed: ");
+
+        return this.scanner.nextInt();
+    }
+
+    public double promptNewPrice() {
+        System.out.println("Enter new base price of rooms: ");
+
+        return this.scanner.nextDouble();
+    }
+
+    public String promptFirstName() {
+        System.out.println("Enter first name of guest: ");
+
+        return this.scanner.nextLine();
+    }
+
+    public String promptLastName() {
+        System.out.println("Enter last name of guest: ");
+
+        return this.scanner.nextLine();
+    }
+
+    public void displayChangeNameInfo(String oldName, String newName) {
+        System.out.pritln("Current Name: " + oldName);
+        System.out.pritln("Will be changed to: " + newName);
+    }
+
+    public void displayChangeNameSuccess() {
+        System.out.println("Changed hotel name successfully.");
+    }
+
+    public void displayChangeNameFailed() {
+        System.out.println("Change hotel name failed.");
+    }
+
+    public void displayAddSingleRoom() {
+        System.out.println("1 room will be added.");
+    }
+
+    public void displayAddRoomSuccess() {
+        System.out.println("1 room successfully added.");
+    }
+
+    public void displayAddRoomFailed() {
+        System.out.println("1 room failed to be added.");
+    }
+
+    public void displayAddMultipleRoom(int size) {
+        System.out.println(size + " rooms will be added.");
+    }
+
+    public void displayAddMultipleRoomSuccess(int size) {
+        System.out.println(size + " rooms successfully added.");
+    }
+
+    public void displayAddMultipleRoomFailed() {
+        System.out.println("Number of rooms to be added exceeds maximum capacity.");
+    }
+
+    public void displayRemoveSingleRoom() {
+        System.out.println("1 room will be removed.");
+    }
+
+    public void displayRemoveRoomSuccess() {
+        System.out.println("1 room successfully removed.");
+    }
+
+    public void displayRemoveRoomFailed() {
+        System.out.println("1 room failed to be removed.");
+    }
+
+    public void displayRemoveMultipleRoom(int size) {
+        System.out.println(size + " rooms will be removed.");
+    }
+
+    public void displayRemoveMultipleRoomSuccess(int size) {
+        System.out.println(size + " rooms successfully added.");
+    }
+
+    public void displayRemoveMultipleRoomFailed() {
+        System.out.println("Hotel can't have zero rooms or all rooms have reservation.");
+    }
+
+    public void displayPromptPriceInvalid() {
+        System.out.println("New price is less than 100.00");
+    }
+
+    public void displayUpdatePriceInfo(double oldPrice, double newPrice) {
+        System.out.println("Current Price: " + oldPrice);
+        System.out.println("New Price: " + newPrice);
+    }
+
+    public void displayUpdatePriceSuccess() {
+        System.out.println("Base price successfully changed.");
+    }
+
+    public void displayUpdatePriceFailed() {
+        System.out.println("Base price can't be changed because there are reservations.");
+    }
+
+    public void displayRemoveReservationInfo(String firstName, String lastName) {
+        System.out.println("Reservation of " + firstName + " " + lastName + " will be removed.");
+    }
+
+    public void displayRemoveReservationSuccess() {
+        System.out.println("Removed reservation successfully.");
+    }
+
+    public void displayRemoveReservationFailed() {
+        System.out.println("Reservation doesn't exist.");
+    }
+
+    public void displayRemoveReservationInfo(String firstName, String lastName) {
+        System.out.println("Reservation of " + firstName + " " + lastName + " will be removed.");
+    }
+
+    public void displayRemoveReservationSuccess() {
+        System.out.println("Removed reservation successfully.");
+    }
+
+    public void displayRemoveReservationFailed() {
+        System.out.println("Reservation doesn't exist.");
+    }
+
+    public void displayRemoveHotelInfo() {
+        System.out.println("You will be removing this hotel.");
+    }
+
+    public void displayRemoveHotelSuccess() {
+        System.out.println("Removed hotel successfully.");
+    }
+
+    public void displayRemoveHotelFailed() {
+        System.out.println("Removing hotel failed.");
+    }
+
+
 }
