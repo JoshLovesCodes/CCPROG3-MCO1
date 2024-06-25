ab3e608495ed0787441cfef1f7baf3bc8ce7c22c
swifted03 <franzcolobong23@gmail.com>
2024-06-25 11:59:17 +0800

    Added availability attribute

--- VERSION 2 (MVC)/Room.java
+++ VERSION 2 (MVC)/Room.java
@@ -1,6 +1,8 @@
 public class Room {
     private String name;
     private double price = 1299.0;
+    private boolean availbility = true;
+
 
     public Room(String name) {
         this.name = name;
@@ -14,7 +16,15 @@ public class Room {
         return this.price;
     }
 
+    public boolean getAvailability() {
+        return this.availbility;
+    }
+
     public void setPrice(double price) {
         this.price = price;
+    }
+
+    public void setAvailability(boolean availbility) {
+        this.availbility = availbility;
     }
 }
