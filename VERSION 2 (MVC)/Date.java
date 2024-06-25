c01a098ea9954d410396898f30954afca01eb322
JoshLovesCodes <67225910+JoshLovesCodes@users.noreply.github.com>
2024-06-25 16:09:24 +0800

    added constructor

--- VERSION 2 (MVC)/Date.java
+++ VERSION 2 (MVC)/Date.java
@@ -9,6 +9,14 @@ public class Date {
         this.minute = minute;
     } 
 
+    public Date(int year, int month, int day) {
+        this.year = year;
+        this.month = month;
+        this.day = day;
+        this.hour = 0;
+        this.minute = 0;
+    } 
+
     public int getYear() {
         return this.year;
     }
@@ -47,5 +55,46 @@ public class Date {
 
     public void setMinute(int newMinute) {
         this.minute = newMinute;
+    }
+
+    public String identifyMonth(int month) {
+        switch(month) {
+            case 1:
+                return "January";
+                break;
+            case 2:
+                return "February";
+                break;
+            case 3:
+                return "March";
+                break;
+            case 4:
+                return "April";
+                break;
+            case 5:
+                return "May";
+                break;
+            case 6:
+                return "June";
+                break;
+            case 7:
+                return "July";
+                break;
+            case 8:
+                return "August";
+                break;
+            case 9:
+                return "September";
+                break;
+            case 10:
+                return "October";
+                break;
+            case 11:
+                return "November";
+                break;
+            case 12:
+                return "December";
+                break;
+        }
     }
 }
