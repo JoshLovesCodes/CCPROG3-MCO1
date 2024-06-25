89a1f17ad4d7357e408506fde36ad64423efe163
JoshLovesCodes <67225910+JoshLovesCodes@users.noreply.github.com>
2024-06-25 11:07:20 +0800

    name checker and create hotel made

--- VERSION 2 (MVC)/MainModel.java
+++ VERSION 2 (MVC)/MainModel.java
@@ -2,4 +2,25 @@ import java.util.ArrayList;
 
 public class MainModel {
     private ArrayList<Hotel> hotelList;
+
+    public boolean nameChecker(String name) {
+        for(Hotel h: this.hotelList) {
+            if(h.getName == name) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public boolean createHotel(String newHotelName) {
+        boolean isNameUnique = nameChecker(newHotelName);
+
+        if(isNameUnique) {
+            this.hotelList.add(new Hotel(newHotelName));
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
